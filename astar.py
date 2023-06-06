f14a6fda2224c1c50c12670214d99bb94f9860fd
Zhijia <zhijiaxzhang@gmail.com>
2023-06-05 22:50:34 -0400

    functional

--- astar.py
+++ astar.py
@@ -1,26 +1,22 @@
 import pygame
-import math 
 from queue import PriorityQueue
-
 
 #setting up visualizer display windowdow
 WIDTH = 800
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
 pygame.display.set_caption("Shortest Path Search Algorithm")
 
-
 #colors for board
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
+AQUAMARINE = (28,134,238)
+AQUA = (16,78,139)
 BLUE = (0, 255, 0)
 YELLOW = (255, 255, 0)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-PURPLE = (128, 0, 128)
+YELLOW = (255,255,0)
 ORANGE = (255, 165 ,0)
 GREY = (128, 128, 128)
 TURQUOISE = (64, 224, 208)
-
 
 """
 Tracks row/column , WIDTH, neighbors, color
@@ -44,10 +40,10 @@ class Node:
         return self.row, self.col
 
     def is_closed(self):
-        return self.color == RED
+        return self.color == AQUAMARINE
     
     def is_open(self): 
-        return self.color == GREEN
+        return self.color == AQUA
     
     def is_barrier(self):
         return self.color == BLACK
@@ -67,10 +63,10 @@ class Node:
         self.color = ORANGE
 
     def make_closed(self):
-        self.color = RED
+        self.color = AQUAMARINE
 
     def make_open(self):
-        self.color = GREEN
+        self.color = AQUA
 
     def make_barrier(self):
         self.color = BLACK
@@ -79,14 +75,32 @@ class Node:
         self.color = TURQUOISE
 
     def make_path(self):
-        self.color = PURPLE
+        self.color = YELLOW
 
     def draw(self, window): 
         pygame.draw.rect(window, self.color, (self.x, self.y, self.width, self.width))
 
 
     def update_neighbors(self, grid):
-        pass
+        self.neighbors = []
+
+        #check node below
+        if self.row < self.total_rows - 1 and not grid[self.row + 1][self.col].is_barrier():
+            self.neighbors.append(grid[self.row + 1][self.col])
+
+        #check node above
+        if self.row > 0 and not grid[self.row -  1][self.col].is_barrier():
+            self.neighbors.append(grid[self.row - 1][self.col])
+
+        #check node right
+        if self.col < self.total_rows - 1 and not grid[self.row][self.col + 1].is_barrier():
+            self.neighbors.append(grid[self.row][self.col + 1])
+
+        #check node left
+        if self.col > 0 and not grid[self.row ][self.col - 1].is_barrier():
+            self.neighbors.append(grid[self.row][self.col - 1])
+
+            
 
 
     def __lt__(self, other):
@@ -104,6 +118,95 @@ def h(point1, point2):
     return abs(x1 - x2) + abs(y1 - y2)
 
 
+
+"""
+Takes in the function draw
+
+"""
+def astar(draw, grid, start, end):
+
+    #count breaks ties between two nodes that have the same f score
+    #tiebreaker = whichever node is inserted first
+    count = 0
+
+    open_set = PriorityQueue()
+
+    open_set.put((0, count, start))
+
+    #keeps track of the previous node of a node (used to backtrack path)
+    previous = {}
+
+    #g score of each node, which is the current shortest distance from the start node to this node
+    g_score = {node: float("inf") for row in grid for node in row}
+    g_score[start] = 0
+    
+    #f score of each node, which is the pAQUAMARINEicted distance of this node to the end node
+    f_score = {node: float("inf") for row in grid for node in row}
+    f_score[start] = 0 + h(start.get_position(), end.get_position())
+
+    #keeps track of what nodes are in the priority queue or not in O(1)
+    open_set_hash = {start}
+
+    #keeps looping as long as the pq is not empty
+    while not open_set.empty():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+
+        #gets the node with the lowest f score from the queue
+        current = open_set.get()[2]
+        open_set_hash.remove(current)
+
+        #if it's the end then we found the shortest path, so need to reconstruct by backtracking
+        if current == end:
+            reconstruct_path(previous, end, draw)
+            end.make_end()
+            return True
+        
+        #iterate through each neighbor
+        for neighbor in current.neighbors:
+
+            #gscore + 1 (because it would be 1 more from the start than the current node)
+            temp_g_score = g_score[current] + 1
+
+            #if the new g score is better than the previous g score it had, update it since we found a better g score
+            if temp_g_score < g_score[neighbor]:
+
+                #record the previous node of the neighbor (which is the current node)
+                previous[neighbor] = current
+
+                #update the g score (because we found a better one)
+                g_score[neighbor] = temp_g_score
+
+                #update f score
+                f_score[neighbor] = temp_g_score + h(neighbor.get_position(), end.get_position())
+
+                #if this neighbor is not in the pq, add it
+                if neighbor not in open_set_hash:
+                    count += 1
+                    open_set.put((f_score[neighbor], count, neighbor))
+                    open_set_hash.add(neighbor)
+                    neighbor.make_open()
+
+        draw()
+
+        if current != start: 
+            current.make_closed()
+
+
+    return False
+
+
+def reconstruct_path(previous, current, draw):
+
+    #eventually terminates because start node is not in start
+    while current in previous:
+        current = previous[current]
+        current.make_path()
+        draw()
+
+                
 
 #makes the grid
 def make_grid(rows, width):
@@ -175,12 +278,7 @@ def main(window, width):
             #event where the "little x" is pressed  
             if event.type == pygame.QUIT: 
                 run = False
-
-            #algorithm running (state of board is LOCKED)
-            if started:
-                continue
-            
-            
+                        
             #left mouse
             if pygame.mouse.get_pressed()[0]:
                 position = pygame.mouse.get_pos()
@@ -216,9 +314,21 @@ def main(window, width):
 
 
 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and start and end:
+                    for row in grid: 
+                        for node in row: 
+                            node.update_neighbors(grid)
 
 
+                    astar(lambda: draw(window, grid, ROWS, width), grid, start, end)
 
+            
+
+                if event.key == pygame.K_c:
+                    start = None
+                    end = None
+                    grid = make_grid(ROWS, width)
     
     pygame.quit()
 
